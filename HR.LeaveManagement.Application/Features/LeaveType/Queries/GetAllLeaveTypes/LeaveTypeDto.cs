86a485e4680c12087b5cd065bfcc5af744a4c0ad
ahmadabdelbary2001 <ahmadabdelbary2001@gmail.com>
2025-07-05 01:27:46 +0300

    add LeaveTypeDto class

--- HR.LeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveTypes/LeaveTypeDto.cs
+++ HR.LeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveTypes/LeaveTypeDto.cs
@@ -2,5 +2,7 @@ namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveT
 
 public class LeaveTypeDto
 {
-    
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int DefaultDays { get; set; }
 }
