59228427d0e62263f9155782fddfc88afc66336e
ahmadabdelbary2001 <ahmadabdelbary2001@gmail.com>
2025-07-05 14:15:37 +0300

    create CQRS command request & handler for delete LeaveType

---
+++ HR.LeaveManagement.Application/Features/LeaveType/Commands/DeleteLeaveType/DeleteLeaveTypeCommandHandler.cs
@@ -0,0 +1,26 @@
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.DeleteLeaveType;
+
+public class DeleteLeaveTypeCommandHandler : IRequestHandler<DeleteLeaveTypeCommand, Unit>
+{
+    private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+    public DeleteLeaveTypeCommandHandler(ILeaveTypeRepository leaveTypeRepository)
+    {
+        _leaveTypeRepository = leaveTypeRepository;
+    }
+
+    public async Task<Unit> Handle(DeleteLeaveTypeCommand request, CancellationToken cancellationToken)
+    {
+        // retrieve domain entity object
+        var leaveTypeToDelete = await _leaveTypeRepository.GetByIdAsync(request.Id);
+        
+        // remove from database
+        await _leaveTypeRepository.DeleteAsync(leaveTypeToDelete);
+
+        // retun record id
+        return Unit.Value;
+    }
+}
