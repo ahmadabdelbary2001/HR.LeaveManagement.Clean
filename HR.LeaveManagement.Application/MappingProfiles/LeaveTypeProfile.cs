8d1a38b15d7d9fdbc08355737076c51a6c2a51fe
ahmadabdelbary2001 <ahmadabdelbary2001@gmail.com>
2025-07-05 12:55:49 +0300

    create CQRS query request & handler for LeaveTypes details

--- HR.LeaveManagement.Application/MappingProfiles/LeaveTypeProfile.cs
+++ HR.LeaveManagement.Application/MappingProfiles/LeaveTypeProfile.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetLeaveTypeDetails;
 using HR.LeaveManagement.Domain;
 
 namespace HR.LeaveManagement.Application.MappingProfiles
@@ -10,6 +11,7 @@ namespace HR.LeaveManagement.Application.MappingProfiles
         public LeaveTypeProfile()
         {
             CreateMap<LeaveTypeDto, LeaveType>().ReverseMap();
+            CreateMap<LeaveType, LeaveTypeDetailsDto>().ReverseMap();
         }
     }
 }
