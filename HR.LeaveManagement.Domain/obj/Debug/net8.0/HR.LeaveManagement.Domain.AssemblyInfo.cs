de05d9739ff9aa8f1ea7d52c25ffe4091273e5ea
ahmadabdelbary2001 <ahmadabdelbary2001@gmail.com>
2025-07-05 15:11:52 +0300

    Implement FluentValidation for LeaveType Commands

--- HR.LeaveManagement.Domain/obj/Debug/net8.0/HR.LeaveManagement.Domain.AssemblyInfo.cs
+++ HR.LeaveManagement.Domain/obj/Debug/net8.0/HR.LeaveManagement.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HR.LeaveManagement.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86a485e4680c12087b5cd065bfcc5af744a4c0ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6dade6f5f831858e77c452b8f240968f3237724")]
 [assembly: System.Reflection.AssemblyProductAttribute("HR.LeaveManagement.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HR.LeaveManagement.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
