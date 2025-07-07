f6811fcb1b0b3b5e80c5ff04e4faa12fb18e2a1c
ahmadabdelbary2001 <ahmadabdelbary2001@gmail.com>
2025-07-08 01:13:39 +0300

    Complete Leave Request Feature (CQRS)

--- HR.LeaveManagement.Application/obj/Debug/net8.0/HR.LeaveManagement.Application.AssemblyInfo.cs
+++ HR.LeaveManagement.Application/obj/Debug/net8.0/HR.LeaveManagement.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HR.LeaveManagement.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bb65985941587ca4947012c9d2e530509f72555")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c38fdaf486249c50db4c698eeb6d3a12f0ab9a74")]
 [assembly: System.Reflection.AssemblyProductAttribute("HR.LeaveManagement.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HR.LeaveManagement.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
