64b1b7a898acb61769ba71102a4e07328a2fe29e
ahmadabdelbary2001 <ahmadabdelbary2001@gmail.com>
2025-07-07 14:05:51 +0300

    Scaffolding Database

--- HR.LeaveManagement.Domain/obj/Debug/net8.0/HR.LeaveManagement.Domain.AssemblyInfo.cs
+++ HR.LeaveManagement.Domain/obj/Debug/net8.0/HR.LeaveManagement.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HR.LeaveManagement.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a493ab1b7e8fb9c099a4dda0c107b2417ace1bb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03d1e29324125e6c86b3ac27b8318e29e0e98a58")]
 [assembly: System.Reflection.AssemblyProductAttribute("HR.LeaveManagement.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HR.LeaveManagement.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
